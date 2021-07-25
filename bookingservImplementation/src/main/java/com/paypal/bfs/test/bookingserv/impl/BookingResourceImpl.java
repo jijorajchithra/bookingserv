4ec7c1f265406cd7db7786e8dd7ceba456dc2cc9
jchithra <jijoraj_chithra@intuit.com>
2021-07-25 12:46:31 +0530

    fix:added the missing RestController annotation

--- bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
+++ bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
@@ -3,10 +3,13 @@ package com.paypal.bfs.test.bookingserv.impl;
 import com.paypal.bfs.test.bookingserv.api.BookingResource;
 import com.paypal.bfs.test.bookingserv.api.model.Booking;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BookingResourceImpl implements BookingResource {
     @Override
     public ResponseEntity<Booking> create(Booking booking) {
-        return null;
+        booking.setId(1);
+        return ResponseEntity.accepted().body(booking);
     }
 }
